6b2f3461b1341828fee386adb84c942549cf41de
Wambani01 <georgewmbn@gmail.com>
2022-11-29 13:02:53 -0800

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     digit = abs(number) % 10
-    print("{:d}".format(digit))
-    return digit 
+    print("{:d}".format(digit), end="")
+    return digit
