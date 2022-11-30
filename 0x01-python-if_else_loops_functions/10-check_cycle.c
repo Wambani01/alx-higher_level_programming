6899892923a1bf859e9ce68f4e6e7660380c378b
Wambani01 <georgewmbn@gmail.com>
2022-11-30 00:31:36 -0800

    task 10

---
+++ 0x01-python-if_else_loops_functions/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * check_cycle - checks for a cycle in a linked list
+ * @list: head of the list
+ * Return: 0 on failure, 1 on success
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+
+	while (slow->next && fast->next->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
