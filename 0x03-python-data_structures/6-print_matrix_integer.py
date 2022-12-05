d576f81ac8961091500d61adf02c732a76bb0802
Wambani01 <georgewmbn@gmail.com>
2022-12-05 04:22:44 -0800

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,12 +9,3 @@ def print_matrix_integer(matrix=[[]]):
                 print("{} ".format(j), end="")
             p += 1
         print()
-matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-        ]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
