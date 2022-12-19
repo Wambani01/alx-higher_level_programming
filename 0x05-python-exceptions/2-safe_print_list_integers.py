9f45fc085321fbdda1eb01f27e34b8fd55c0ac9a
Wambani01 <georgewmbn@gmail.com>
2022-12-19 10:41:21 -0800

    Type Error handling

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             counter += 1
-        except (TypeError, IndexError):
+        except TypeError:
             pass
     print()
     return (counter)
