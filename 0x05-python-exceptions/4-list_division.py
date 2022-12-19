42a1cc73c8cafe2d15797bc8dbca0bbc5f97c4ab
Wambani01 <georgewmbn@gmail.com>
2022-12-19 13:15:19 -0800

    multiple error handling

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,9 +11,8 @@ def list_division(my_list_1, my_list_2, list_length):
             div = 0
             print("division by 0")
         except IndexError:
+            div = 0
             print("out of range")
         finally:
             new_list.append(div)
     return(new_list)
-
-
