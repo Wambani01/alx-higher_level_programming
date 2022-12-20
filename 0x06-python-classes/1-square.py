08940914a786e2959dec40d8056ea90ea687914d
Wambani01 <georgewmbn@gmail.com>
2022-12-20 10:38:10 -0800

    created a square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines a square"""
+
+class Square:
+    """Defined square class"""
+    def __init__(self, size):
+        """intantiated square class
+        Args: size - the size of the square defined
+        """
+        self.__size = size
