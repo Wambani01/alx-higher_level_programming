ddfd5d698a45717a7e77a13d982a5c9d8a348181
Wambani01 <georgewmbn@gmail.com>
2022-12-20 11:05:14 -0800

    multiple square class with exception

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,7 +15,7 @@ class Square:
         """
         if size < 0:
             raise ValueError("size must be >= 0")
-        elif !isinstance(size, int):
+        elif not isinstance(size, int):
             raise TypeError("size must be an integer")
         else:
             self.__size = size
