cc7aea278fa6810baca5eec938ac7aefc3d048bf
Wambani01 <georgewmbn@gmail.com>
2022-12-21 05:32:23 -0800

    added set and getter for position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,7 +23,7 @@ class Square:
         if not isinstance(value, tuple) or\
             not isinstance(value[0], int) or\
             not isinstance(value[1], int) or len(value) != 2\
-                or value[1] < 0 and value[0] < 0:
+                or value[1] < 0 or value[0] < 0:
             raise TypeError("position must be a tuple of\
             2 positive integers")
         else:
