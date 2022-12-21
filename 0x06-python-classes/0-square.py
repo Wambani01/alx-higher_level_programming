2924bbd579dbdade912031560f643bb0826b2040
Wambani01 <georgewmbn@gmail.com>
2022-12-21 06:03:36 -0800

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+""" python3 -c 'print(__import__("my_module").__doc__)' """
+
+
 class Square:
-    """Defined an empty class"""
+    """ python3 -c 'print(__import__("my_module").MyClass.__doc__)' """
+
     pass
