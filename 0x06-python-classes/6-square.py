5dc481df587ef53262ddf0b30eaa68e28d2d5ad0
Wambani01 <georgewmbn@gmail.com>
2022-12-21 05:42:50 -0800

    added set and getter for position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,25 +12,25 @@ class Square:
         self.__size = size
         self.__position = position
 
-    @property                                                                                   
-    def size(self):                                                                             
-        """getter for size"""                                                                   
-        return self.__size                                                                      
-                                                                                                
-    @size.setter                                                                                
-    def size(self, value):                                                                      
-        """setter for size                                                                      
-                                                                                                
-        Raises:                                                                                 
-        TypeError: if size is not type int                                                      
-        ValueError: if size is less than zero                                                   
-        """                                                                                     
-        if not isinstance(value, int):                                                          
-            raise TypeError("size must be an integer")                                          
-        elif value < 0:                                                                         
-            raise ValueError("size must be >= 0")                                               
-        else:                                                                                   
-            self.__size = value  
+    @property
+    def size(self):
+        """getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter for size
+
+        Raises:
+        TypeError: if size is not type int
+        ValueError: if size is less than zero
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @property
     def position(self):
@@ -40,9 +40,9 @@ class Square:
     @position.setter
     def position(self, value):
         """setter for position"""
-        if not isinstance(value, tuple) or\
-            not isinstance(value[0], int) or\
-            not isinstance(value[1], int) or len(value) != 2\
+        if not isinstance(value, tuple) or len(value) != 2\
+            or not isinstance(value[0], int) or\
+            not isinstance(value[1], int)\
                 or value[1] < 0 or value[0] < 0:
             raise TypeError("position must be a tuple of\
             2 positive integers")
