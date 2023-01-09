a9433c63fe8c98b2288e64ea552a902bfc6e2d5c
Wambani01 <georgewmbn@gmail.com>
2023-01-09 04:48:27 -0800

    defining a class

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
     def area(self):
         """riases exception error"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """validates value"""
         if type(value) is not int:
