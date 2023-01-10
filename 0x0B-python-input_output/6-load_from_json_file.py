25dd4fcf5a933a289fb541fe371ec135d5358a91
Wambani01 <georgewmbn@gmail.com>
2023-01-10 10:32:29 -0800

    adding to jason file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 def load_from_json_file(filename):
     """function to load from a json file"""
 
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,6 +8,10 @@ import json
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
 my_list = []
-my_list = my_list + sys.argv[1:]
-save(my_list, "add_item.json")
-load("add_item.json") 
+try:
+    my_list = load("add_item.json")
+except FileNotFoundError:
+    save(sys.argv[1:], "add_item.json")
+else:
+    my_list = my_list.extend(sys.argv[1:])
+    save(my_list, "add_item.json")
