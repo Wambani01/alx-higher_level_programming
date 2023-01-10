40f35fdd026946e0417d7816a3d00a8bfd1c8dd9
Wambani01 <georgewmbn@gmail.com>
2023-01-10 04:51:00 -0800

    reading from jason file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""read from a jason file"""
+import json
+
+
+
+def load_from_json_file(filename):
+    """function to load from a json file"""
+
+    with open(filename, encoding="utf-8") as f:
+        return json.loads(f.read())
