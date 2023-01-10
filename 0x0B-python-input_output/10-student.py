4398069c1422d9b7612e5bdd18a518467d77dae1
Wambani01 <georgewmbn@gmail.com>
2023-01-10 12:49:43 -0800

    serialization

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""""instantiates a class and converts its items
+to json representation"""
+
+
+class Student:
+    """defines a student including a method to
+    represent the class in json"""
+
+    def __init__(self, first_name, last_name, age):
+        """instantiates the class object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """converts class attributes to jason
+        representation"""
+
+       if isinstance(attrs, list) and\
+            all(isinstance(item, str) for item in attrs):
+            result = {}
+            for i in attrs:
+                try:
+                    result[i] = self.__dict__[i]
+                except Exception:
+                    pass
+            return result
+        return self.__dict__ 
