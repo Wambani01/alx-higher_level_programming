363e7b5e2ccfcf4cfaa173393c5ff518a1c74443
Wambani01 <georgewmbn@gmail.com>
2023-01-10 04:18:55 -0800

    converting jason to python object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+"""return a python object from json string"""
+import json
+
+
+def from_json_string(my_str):
+    """returns python object from a jason string"""
+
+    return json.loads(my_str)
