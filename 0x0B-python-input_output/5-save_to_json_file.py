a70167235888258f197921f66b84b944955cf37e
Wambani01 <georgewmbn@gmail.com>
2023-01-10 04:30:58 -0800

    writing to a json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""writes jason representation to a file"""
+
+
+def save_to_json_file(my_obj, filename):
+    """saves python object to json file"""
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
