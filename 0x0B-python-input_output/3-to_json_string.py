4ff37323be1ca156a3ff997964c9f8fb88c17562
Wambani01 <georgewmbn@gmail.com>
2023-01-10 04:06:34 -0800

    string to object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+
+"""returns json representation of a string"""
+
+
+def to_json_string(my_obj):
+    """functions to convert json to string"""
+
+    return json.dumps(my_obj)
+
