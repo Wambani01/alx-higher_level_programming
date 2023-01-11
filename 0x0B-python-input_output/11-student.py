b922fe21ebffc95d0c5f86cad4ddb3542106052f
Wambani01 <georgewmbn@gmail.com>
2023-01-11 01:19:37 -0800

    deserialization

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+""""instantiates a class and converts its items
+to json representation"""
+
+
+class Student:
+    """defines a student including a method to
+    represent the class in json"""
+
+    def __init__(self, first_name, last_name, age):
+        """instantiates the class object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """converts class attributes to jason
+        representation"""
+        if isinstance(attrs, list) and\
+                all(isinstance(item, str) for item in attrs):
+            result = {}
+            for i in attrs:
+                try:
+                    result[i] = self.__dict__[i]
+                except Exception:
+                    pass
+            return result
+        elif attrs is None:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """deserialization"""
+
+        for i in json.keys():
+            self.__dict__[i] = json[i]
+        return self.__dict__
