3585ba266a46b59d04d7738bee01877a7225bd97
Wambani01 <georgewmbn@gmail.com>
2023-01-11 04:02:50 -0800

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+"""returns a list of pascal triangle
+coefficients"""
+
+
+def pascal_triangle(n):
+    """function to return pascal triangle
+    coeffecients as list of lists"""
+
+    
+    if n < 1:
+        my_list = []
+        return my_list
+    else:
+        list_i= [[1]]
+        for i in range(n - 1):
+            temp = [0] + list_i[-1] + [0]
+
+            row = []
+
+            for j in range(len(list_i[-1]) + 1):
+                row.append(temp[j] + temp[j + 1])
+            list_i.append(row)
+        return list_i
