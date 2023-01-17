b72088177099399d6c3c47331ee662b90e6e021c
Wambani01 <georgewmbn@gmail.com>
2023-01-17 01:08:25 -0800

    python classes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,7 +92,7 @@ class Rectangle(Base):
     def __str__(self):
         """returns a string representation of the object"""
 
-        return (f"[Rectangle] ({self.id}){self.__x}/{self.__y}\
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y}\
  - {self.__width}/{self.__height}")
 
     def update(self, *args, **kwargs):
