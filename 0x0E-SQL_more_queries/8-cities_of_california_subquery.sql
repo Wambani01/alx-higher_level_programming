3de8b958f84bf255ab09b842855795d1ffb834f4
Wambani01 <georgewmbn@gmail.com>
2023-02-15 11:50:55 -0800

    nested query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- nested select statements
-SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = "California")
-    ORDER BY cities.id;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+    ORDER BY id;
