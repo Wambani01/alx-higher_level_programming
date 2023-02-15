d62cc9e62f68f738971cab9f281b5a27dede0775
Wambani01 <georgewmbn@gmail.com>
2023-02-15 10:07:14 -0800

    table with default value

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- a table with a default value
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT = 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
