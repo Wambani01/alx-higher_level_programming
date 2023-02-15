1372056038d61bb53ea44993ce444bd37b5308de
Wambani01 <georgewmbn@gmail.com>
2023-02-15 11:13:25 -0800

    foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- foreign key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
     PRIMARY KEY (id),
