137f25f7c0eb0f88bc1ad775d96bb3d44081da8d
Wambani01 <georgewmbn@gmail.com>
2023-02-16 01:20:28 -0800

    nested left join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,2 +1,8 @@
 -- nested inner left
-select tv_shows.title, tv_genres.name from tv_shows left join tv_show_genres on tv_shows.id = tv_show_genres.show_id left join tv_genres on tv_show_genres.genre_id = tv_genres.id;
+SELECT tv_shows.title, tv_genres.name FROM tv_shows 
+    LEFT JOIN tv_show_genres 
+    ON tv_shows.id = tv_show_genres.show_id 
+    LEFT JOIN tv_genres 
+    ON tv_show_genres.genre_id = tv_genres.id
+    ORDER BY tv_shows.title, tv_genres.name
+;
