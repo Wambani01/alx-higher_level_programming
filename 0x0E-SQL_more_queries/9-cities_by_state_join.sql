73813fc49e6a32739b4b427f58afbddb7e404947
Wambani01 <georgewmbn@gmail.com>
2023-02-16 01:02:12 -0800

    joining several tables

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- using join in sql
-SELECT cities.id, cities.name, states.name FROM cities
-    INNER JOIN states
+SELECT cities.id, cities.name, states.name FROM states
+    INNER JOIN cities
     ON cities.state_id = states.id
     ORDER BY cities.id
 ;
