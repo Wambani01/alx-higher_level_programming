b0f501972b2b57739e5ea521da2df99cd21b0369
Wambani01 <georgewmbn@gmail.com>
2023-03-13 14:13:35 -0700

    max

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (process.argv.length < 4) {
   console.log('0');
 } else {
   const array = process.argv.slice(2, process.argv.length);
-  array.sort(function(a, b){return a - b});
+  array.sort(function (a, b) { return a - b; });
   array.pop();
   const max = Math.max(...array);
   console.log(max);
