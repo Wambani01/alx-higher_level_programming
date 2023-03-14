3ebae4a3e5a3ff23277c64e70df254fb3e35aaff
Wambani01 <georgewmbn@gmail.com>
2023-03-14 11:49:54 -0700

    inheritance

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const oldSq = require('./5-square');
+class Square extends oldSq {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
