ce00a30644a18f0ce8f3e2fdf769bb01de36c857
Wambani01 <georgewmbn@gmail.com>
2023-03-17 15:13:02 -0700

    fixed styling

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """ sql alchemy """
-from sqlalchemy import create_engine, Sequence, Column, Integer, String, ForeignKey
+from sqlalchemy import Sequence, Column, Integer, String, ForeignKey
 from model_state import Base, State
 
 
