e58cad3bbcfde1683e64d5b316c699195910f98e
Wambani01 <georgewmbn@gmail.com>
2023-03-31 01:48:45 -0700

    using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# a python script to open a url
+
+from urllib.request import urlopen
+
+with urlopen("https://alx-intranet.hbtn.io/status") as f:
+    body = f.read()
+print('Body response:')
+print(f'    - type: {type(body)}')
+print(f'    - content: {body}')
+print(f'    - utf8 content: {body.decode("utf-8")}')
