04189f77b402d5e919e33a9266c0067a94cd26c2
Wambani01 <georgewmbn@gmail.com>
2023-03-31 07:06:04 -0700

    fixed errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         print('Body response:')
         print(f'\t- type: {type(body)}')
         print(f'\t- content: {body}')
-        print(f'\t - utf8 content: {body.decode("utf-8")}')
+        print(f'\t- utf8 content: {body.decode("utf-8")}')
