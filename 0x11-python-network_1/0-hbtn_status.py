763b7be92780c4620ddd8745d34153cbaf7ea5a7
Wambani01 <georgewmbn@gmail.com>
2023-03-31 04:44:49 -0700

    get request with requests package

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urlopen("https://alx-intranet.hbtn.io/status") as f:
         body = f.read()
         print('Body response:')
-        print(f'    - type: {type(body)}')
-        print(f'    - content: {body}')
-        print(f'    - utf8 content: {body.decode("utf-8")}')
+        print(f'\t- type: {type(body)}')
+        print(f'\t- content: {body}')
+        print(f'\t - utf8 content: {body.decode("utf-8")}')
