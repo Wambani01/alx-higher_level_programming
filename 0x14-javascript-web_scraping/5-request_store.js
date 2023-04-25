ee2f515fce85d5575a8ffa531010f25e76a5c314
Wambani01 <georgewmbn@gmail.com>
2023-04-25 06:05:53 -0700

    writing to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,10 +2,10 @@
 
 // a script to writr to a file
 
-const request = require('request')
+const request = require('request');
 const fs = require('fs');
 const filePath = process.argv[3];
-const url = process.argv[2]
+const url = process.argv[2];
 
 request(url, (err, res, body) => {
   if (err) console.error(err);
