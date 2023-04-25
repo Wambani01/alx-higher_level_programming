836e79de018b5229c00bf0e3fa7dbf7a728796b2
Wambani01 <georgewmbn@gmail.com>
2023-04-25 04:53:57 -0700

    http get request in js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 // printing the status code of a GET request
 
 const request = require('request')
-const url = process.argv[2]
+const url = process.argv[2];
 request(url, (err, res, body) => {
   if (err) console.error(err);
   console.log('code:', res.statusCode);
