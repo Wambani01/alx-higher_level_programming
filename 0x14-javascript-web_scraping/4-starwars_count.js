3e60b0d8fbde7e431e7701db71399ad13434d0ec
Wambani01 <georgewmbn@gmail.com>
2023-04-25 05:46:55 -0700

    api call in js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,8 +10,10 @@ request(url, (err, res, body) => {
   let count = 0;
   for (let i = 0; i < data.results.length; i++) {
     const chars = data.results[i].characters;
-    if (chars.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      count = count + 1;
+    for(let k = 0; k < chars.length; k++) {
+      if (chars[k].includes('18')) {
+        count = count + 1;
+      }
     }
   }
   console.log(count);
